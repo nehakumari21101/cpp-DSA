4fbbb7e3f53acf00c15052f951306e48109a0c5c
Neha kumari <kumarineha122162@gmail.com>
2024-10-24 23:08:59 +0530

    add files

--- 07_Arrays/arrays.cpp
+++ 07_Arrays/arrays.cpp
@@ -1,15 +1,20 @@
 #include<iostream>
 using namespace std;
 
-int main(){
-    int arr[5] = {9, 5, 11, 1, 0};
-    int len = sizeof(arr) / sizeof(int);
+
+void printArr(int arr[], int len){
     for(int i=0; i<len; i++){
         cout<<arr[i]<<" ";
     }
     cout<<endl;
+}
 
-    //Largest in an array
+int main(){
+    int arr[5] = {9, 5, 11, 1, 0};
+    int len = sizeof(arr) / sizeof(int);
+    printArr(arr, len);
+
+    // Largest in an array
     int max = arr[0];
     for(int i =0; i<len; i++){
         if(arr[i]>max){
