2a535174d64464ac4dc9ff1451f13a6274e1db05
Neha kumari <kumarineha122162@gmail.com>
2024-10-29 16:23:21 +0530

    add files

--- 07_Arrays/arrays.cpp
+++ 07_Arrays/arrays.cpp
@@ -9,18 +9,58 @@ void printArr(int arr[], int len){
     cout<<endl;
 }
 
-int main(){
-    int arr[5] = {9, 5, 11, 1, 0};
-    int len = sizeof(arr) / sizeof(int);
-    printArr(arr, len);
-
-    // Largest in an array
+void findLargest(int arr[], int n){
     int max = arr[0];
-    for(int i =0; i<len; i++){
+    for(int i =0; i<n; i++){
         if(arr[i]>max){
             max = arr[i];
         }
     }
     cout<<"max = "<<max<<endl;
+}
+
+int linearSearch(int arr[], int n, int key){
+    for(int i=0; i<n; i++){
+        if(key == arr[i]){
+            return i;
+        }
+        
+    }
+    return -1;
+}
+
+void reverse(int *arr, int n){
+    // int start = 0, end = n-1;
+    // while(start<end){
+    //     swap(start, end);
+    //     start++;
+    //     end--;
+    // }
+
+    for(int i=0; i<n/2; i++){
+        int temp = arr[i];
+        arr[i] = arr[n-i-1];
+        arr[n-i-1] = temp;
+    }
+
+    for(int i=0; i<n; i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+   
+}
+
+int main(){
+    int arr[] = {9, 5, 11, 1, 0, 2};
+    int len = sizeof(arr) / sizeof(int);
+
+    printArr(arr, len);
+    findLargest(arr, len);
+     
+    cout<<linearSearch(arr, len, 2)<<endl;
+
+    reverse(arr, len);
+     
+    
     return 0;
 }
