03d582fc0165d27649d7af2087889ed95479dcfb
Neha kumari <kumarineha122162@gmail.com>
2024-10-31 10:16:20 +0530

    add files

--- 07_Arrays/arrays2.cpp
+++ 07_Arrays/arrays2.cpp
@@ -16,6 +16,7 @@ void printSubarrays(int *arr, int n){
     }
 }
 
+// max sum subarray (Brute force - o(n^3))
 void maxSubarraySum1(int *arr, int n){
     int maxSum = INT_MIN;
     for(int start=0; start<n; start++){
@@ -25,9 +26,6 @@ void maxSubarraySum1(int *arr, int n){
                 sum += arr[i];
             }
             cout<<sum<<", ";
-            // if(maxSum < Sum){
-            //     maxSum = Sum;
-            // }
             maxSum = max(sum, maxSum);
         }
         cout<<endl;
@@ -35,12 +33,40 @@ void maxSubarraySum1(int *arr, int n){
     cout << "maximum subarray sum = "<<maxSum<<endl;
 }
 
+void maxSubarraySum2(int *arr, int n){
+    int maxSum = INT_MIN;
+    for(int start=0; start<n; start++){
+        int sum = 0;
+        for(int i=start; i<n; i++){
+            sum += arr[i];
+            maxSum = max(sum, maxSum);
+        }
+        // cout<<endl;
+    }
+    cout << "maximum subarray sum = "<<maxSum<<endl;
+}
+
+void maxSubarraySum3(int *arr, int n){
+    int maxSum = INT_MIN;
+        int sum = 0;
+        for(int i=0; i<n; i++){
+            sum += arr[i];
+            maxSum = max(sum, maxSum);
+            if(sum<0){
+                sum=0;
+            }
+    }
+    cout << "maximum subarray sum = "<<maxSum<<endl;
+}
+
 int main(){
-    int arr[]={1, 2, 3, 4, 5};
+    int arr[]={-6, 2, -3, 4, 5};
     int n = sizeof(arr)/sizeof(int);
 
     printSubarrays(arr, n);
     maxSubarraySum1(arr, n);
+    maxSubarraySum2(arr, n);
+    maxSubarraySum3(arr, n);
 
     return 0;
 }
